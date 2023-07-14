fd7bebb2737cc9194d2a39c61de06d855f6394e4
ardaocakkk <ardaocak44@gmail.com>
2023-07-14 18:18:14 +0300

    hibernate/jpa mappings

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/ocakarda/cruddemo/dao/AppDAO.java
@@ -0,0 +1,7 @@
+package com.ocakarda.cruddemo.dao;
+
+import com.ocakarda.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/ocakarda/cruddemo/dao/AppDaoImpl.java
@@ -0,0 +1,27 @@
+package com.ocakarda.cruddemo.dao;
+
+import com.ocakarda.cruddemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+public class AppDaoImpl implements AppDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+
+    }
+}
